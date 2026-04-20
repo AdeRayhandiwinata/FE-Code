6a5a2fb7f01ccf37154a5a18b2fc4db73f07f720
AdeRayhandiwinata <2413026@students.universitasmulia.ac.id>
2026-04-20 12:04:46 +0800

    feat: new learn page

---
+++ my-app/app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function LearnPage() {
+    return(
+        <div>
+            <div>
+                <img src="palette.svg"></img>
+                <p>Design</p>
+            </div>
+            <div>
+                <p>Figma</p>
+                <p>8 Hours ago</p>
+            </div>
+        </div>
+    )
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Ade_Rayhan
           </a>
         </div>
       </main>
